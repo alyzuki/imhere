4812f41a4c208dceeb51298da9856f0232f1fcc2
zuki <hieu.leminh@icdrec.edu.vn>
2017-03-22 16:35:16 -0400

    add json and remove this method

--- ImHere/app/src/main/java/com/here/zuki/imhere/Utils/PlaceObject.java
+++ ImHere/app/src/main/java/com/here/zuki/imhere/Utils/PlaceObject.java
@@ -251,28 +251,28 @@ public class PlaceObject {
                             break;
                     }
 
-                    // products found
-                    // Getting Array of Products
-                    products = json.getJSONArray(TAG_PRODUCTS);
-
-                    // looping through All Products
-                    for (int i = 0; i < products.length(); i++) {
-                        JSONObject c = products.getJSONObject(i);
-
-                        // Storing each json item in variable
-                        String id = c.getString(TAG_PID);
-                        String name = c.getString(TAG_NAME);
-
-                        // creating new HashMap
-                        HashMap<String, String> map = new HashMap<String, String>();
-
-                        // adding each child node to HashMap key => value
-                        map.put(TAG_PID, id);
-                        map.put(TAG_NAME, name);
-
-                        // adding HashList to ArrayList
-                        productsList.add(map);
-                    }
+//                    // products found
+//                    // Getting Array of Products
+//                    products = json.getJSONArray(TAG_PRODUCTS);
+//
+//                    // looping through All Products
+//                    for (int i = 0; i < products.length(); i++) {
+//                        JSONObject c = products.getJSONObject(i);
+//
+//                        // Storing each json item in variable
+//                        String id = c.getString(TAG_PID);
+//                        String name = c.getString(TAG_NAME);
+//
+//                        // creating new HashMap
+//                        HashMap<String, String> map = new HashMap<String, String>();
+//
+//                        // adding each child node to HashMap key => value
+//                        map.put(TAG_PID, id);
+//                        map.put(TAG_NAME, name);
+//
+//                        // adding HashList to ArrayList
+//                        productsList.add(map);
+//                    }
                 } else {
                     // no products found
                     // Launch Add New product Activity
